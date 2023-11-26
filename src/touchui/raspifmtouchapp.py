b5b439e7db92b0ade74fe68ef7a4633b6e7ef9f7
Drexel2k <13720303+Drexel2k@users.noreply.github.com>
2023-11-26 12:35:36 +0100

    Initialized touch ui, added PyGObject.

--- src/touchui/raspifmtouchapp.py
+++ src/touchui/raspifmtouchapp.py
@@ -1,1 +1,21 @@
-pass
+import gi
+
+gi.require_version("Gtk", "4.0")
+from gi.repository import Gtk
+
+def on_activate(app):
+    # … create a new window…
+    win = Gtk.ApplicationWindow(application=app)
+    # … with a button in it…
+    btn = Gtk.Button(label='Hello, World!')
+    # … which closes the window when clicked
+    btn.connect('clicked', lambda x: win.close())
+    win.set_child(btn)
+    win.present()
+
+# Create a new application
+app = Gtk.Application(application_id='fm.raspi.raspiFM')
+app.connect('activate', on_activate)
+
+# Run the application
+app.run(None)
